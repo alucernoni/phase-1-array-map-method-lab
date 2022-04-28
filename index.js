0b8c7bd2ccea35d9836e0c5f8a46d409bcfed755
Annemarie <a.lucernoni0@gmail.com>
2022-04-27 21:25:20 -0700

    commit tree a0ad89c5e1788c3752c8d2a0ab7276f7b991abdc

--- index.js
+++ index.js
@@ -12,5 +12,32 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((str) => str.split(' ').map((word) => word[0].toUpperCase() + word.slice(1)).join(' '))
 }
+ 
+
+// const titleCaseWord = word => word[0].toUpperCase() + word.slice(1)
+// const titleCaseSent = (str) => str.split(' ').map(titleCaseWord).join(' ')
+
+
+// const titleCased = () => tutorials.map(titleCaseSent)
+
+// const titleCased = () => tutorials.map((str) => str.split(' ').map(titleCaseWord).join(' '))
+
+
+console.log(titleCased())
+
+
+// const titleCased = () => {
+//   const newArray = tutorials.map((str) => str.split(' '));
+//   for (let element of newArray) {
+//     const x = element.map((word) => {
+//       word[0] = word[0].toUpperCase() + word.slice(1);
+//     });
+//     const joinWord = x.join(' ');
+//   }
+//   return newArray
+// }
+
+
+// console.log(titleCased())
